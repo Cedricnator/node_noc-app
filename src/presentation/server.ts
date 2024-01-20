e868f0bc921978b43fcfe0213c3ff4e4d34bc1ef
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-01-20 10:57:57 -0300

    feat(cron): impl adapter patern
    Se implemento el patron adaptador para asi desacoplar la dependencias de terceros de nuestra aplicacion, siendo asi llamada en nuestro app.ts.
    
    En el cron.service.ts, lo que hice fue definir dos tipos de datos, un tipo de dato(cronTime) me permitirá definir el tiempo en el que se va a disparar la funcion y el segundo dato(onTick) me permite definir la funcion. En este caso, onTick es simplemente un console.log que nos dirá que cada cierto tiempo se esta ejecutando el cronTime.
    
    cron.service.ts, lo llamamos en nuestro server.ts. Siendo asi que cada vez que el servidor arranca, se ejecutará el cron que hemos adaptado.

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,32 @@
+import { CronService } from "./cron/cron.service"
+
 export class Server {
     public static start() {
         console.log('Server started...')
+        CronService.createJob(
+            '* * * * * *',
+            () => {
+                const date = new Date();
+                console.log('You will see this message every second', date.toLocaleTimeString('es-CL'))
+            }
+        );
+
+        CronService.createJob(
+            '*/2 * * * * *',
+            () => {
+                const date = new Date();
+                console.log('You will see this message every 2 seconds', date.toLocaleTimeString('es-CL'))
+            }
+        );
+
+        CronService.createJob(
+            '*/3 * * * * *',
+            () => {
+                const date = new Date();
+                console.log('You will see this message every 3 seconds', date.toLocaleTimeString('es-CL'))
+            }
+        );
+
     }
+
 }
