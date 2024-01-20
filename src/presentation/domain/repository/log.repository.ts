1bac51402bafc8d009b73dc6c81b5238a0586bf2
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-01-20 13:28:48 -0300

    feat(repository): impl the logRepository

---
+++ src/presentation/domain/repository/log.repository.ts
@@ -0,0 +1,8 @@
+import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
+
+// Permite llamar métodos que se encuentran en dataSource
+export abstract class LogRepository {
+    abstract saveLog( log: LogEntity ): Promise<void>;
+    abstract getLogs( severityLevel: LogSeverityLevel): Promise<LogEntity[]>;
+}
+
